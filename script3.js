6018eb40c142c90d8413d579973e31b6d7bc62ee
Elena <goldmann.elena@gmail.com>
2022-04-12 20:48:09 +0300

    fix deep copy

--- script3.js
+++ script3.js
@@ -1,16 +1,51 @@
 // 3.Object deep clone
-const deepCopy = (obj) => JSON.parse(JSON.stringify(obj));
+// const deepCopy = (obj) => JSON.parse(JSON.stringify(obj));
+
+// const person = {
+//     name: {
+//         first: 'John',
+//         last: 'Doe'
+//     }
+// };
+
+// const anotherPerson = deepCopy(person);
+// anotherPerson.name.first = 'Jane';
+// anotherPerson.name.last = 'Groe';
+
+// console.log(person);
+// console.log(anotherPerson);
+
+const deepCopy = (obj) => {
+    let copy, value;
+  
+    if (typeof obj !== "object" || obj === null) {
+      return obj; 
+    }
+  
+    copy = Array.isArray(obj) ? [] : {};
+  
+    for (let key in obj) {
+      value = obj[key];
+      copy[key] = deepCopy(value);
+    }
+  
+    return copy;
+};
 
 const person = {
     name: {
         first: 'John',
         last: 'Doe'
-    }
+    },
+    func: () => console.log(5)
 };
 
-const anotherPerson = deepCopy(person);
-anotherPerson.name.first = 'Jane';
-anotherPerson.name.last = 'Groe';
+const personCopy = deepCopy(person);
+personCopy.name.first = 'Jane';
+personCopy.func = () => console.log(1);
 
-console.log(person);
-console.log(anotherPerson);
+person.func();
+personCopy.func();
+
+
+
